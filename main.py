b7742fbbcf89e969a7d950dc0a27d6f17f4eab91
SEKI-YUTA <zard.since1991@gmail.com>
2022-06-02 12:40:02 +0900

    add some small program in test.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from cv2 import imshow, imwrite
+from matplotlib.pyplot import text
 import numpy as np
 import cv2 as cv
 
@@ -7,6 +8,7 @@ def main():
     imageCount = 0
     frameCount = 0
     saveDir = "C:/Users/yuta/Desktop/autoEditpg/exported/"
+    videoFPS = 30
 
     # 保存先のディレクトリが存在しなければ作成
     # この処理をしないとimwriteでエラーが出ないのに保存されないという状態になる
@@ -17,6 +19,8 @@ def main():
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
+    
+    videoFPS = cap.get(cv.CAP_PROP_FPS)
     while cap.isOpened():
         # Capture frame-by-frame
         ret, frame = cap.read()
@@ -28,23 +32,27 @@ def main():
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         # Display the resulting frame
         # cv.imshow('frame', gray)
-        if(frameCount > 300):
+        if(frameCount > videoFPS * 20):
             imageArr = np.asarray(frame)
             # パスの区切りに//を使わないとエラーが出るものがあったがこれは/でも大丈夫みたい
             cv.imwrite(saveDir + "exportedImage" + str(imageCount) + ".png", imageArr)
             print("saveing image at " + saveDir + "exportedImage" + str(imageCount) + ".png")
-            imshow("frame", frame)
             imageCount = imageCount + 1
             frameCount = 0
         else:
             frameCount = frameCount + 1
             print("frameCount: " + str(frameCount))
+        putFPSText(frame=frame, text="FPS" + str(videoFPS))
+        imshow("frame", frame )
         if cv.waitKey(1) == ord('q'):
             break
     # When everything done, release the capture
     cap.release()
     cv.destroyAllWindows()
     
+    
+def putFPSText(frame, text):
+    cv.putText(frame, text,(10,20), cv.FONT_HERSHEY_PLAIN, 1, (255,0,0), 2, cv.LINE_4)
     
 if(__name__ == "__main__"):
     main()
--- test.py
+++ test.py
@@ -2,10 +2,80 @@ import sys
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("C://Users//yuta//Desktop//autoEditpg//test.jpg")
+# 画像表示プログラム
+# img = cv.imread("C://Users//yuta//Desktop//autoEditpg//test.jpg")
 
-if img is None:
-    sys.exit()
+# if img is None:
+#     sys.exit()
     
-cv.imshow("image", img)
-k = cv.waitKey(0)
+# cv.imshow("image", img)
+# k = cv.waitKey(0)
+# 画像表示プログラムここまで
+
+
+# webカメラのフレームレートを取得するプログラム
+# cap = cv.VideoCapture(0)
+# print(cap.get(cv.CAP_PROP_FPS))
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     imshow("frame", frame)
+    
+#     if(cv.waitKey(1) == ord("q")):
+#         break
+    
+# cap.release()
+# cv.destroyAllWindows()
+# webカメラのフレームレートを取得するプログラムここまで
+
+
+# 動画ファイルのフレームレートを取得するプログラム
+# cap = cv.VideoCapture("./video_test.mp4")
+# print(cap.get(cv.CAP_PROP_FPS))
+
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     imshow("frame", frame)
+#     if(cv.waitKey(1) == ord("q")):
+#         break
+    
+# cap.release()
+# cv.destroyAllWindows()
+# 動画ファイルのフレームレートを取得するプログラムここまで
+
+
+# 画像を横に並べて表示させるプログラム
+# img1 = cv.imread("./assets/assetImg1.png")
+# img2 = cv.imread("./assets/assetImg2.png")
+
+# mergedImg = np.hstack((img1, img2))
+# print(type(mergedImg))
+# print(type(img1))
+# cv.imshow("img", mergedImg)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+# 画像を横に並べて表示させるプログラムここまで
+
+
+# 画像を完全一致か判定するプログラム
+# img1 = cv.imread("./assets/assetImg1.png")
+# img2 = cv.imread("./assets/assetImg2.png")
+
+# print("画像が一致しています" if np.array_equal(img1, img2) else "画像が一致しません")
+# 画像を完全一致か判定するプログラムここまで
+
+# 2枚の画像の差分画像を書き出すプログラム
+img1 = cv.imread("./assets/assetImg1.png")
+img2 = cv.imread("./assets/assetImg2.png")
+
+if img1.shape == img2.shape:
+    img_diff = img1.astype(int) - img2.astype(int)
+    img_diff_abs = np.abs(img_diff)
+    print(type(img1))
+    print(type(img_diff_abs))
+    # img_diff_abs_grey = cv.cvtColor(img_diff.asType(np.float32), cv.COLOR_BGR2GRAY)
+    print("最大値" + str(img_diff_abs.max()))
+    print("最小値" + str(img_diff_abs.min()))
+    # print(type(img_diff_abs_grey))
+    cv.imwrite("diff_image.png", img_diff_abs)
+    # cv.imshow("diff_image", img_diff_abs)
+# 2枚の画像の差分画像を書き出すプログラムここまで
