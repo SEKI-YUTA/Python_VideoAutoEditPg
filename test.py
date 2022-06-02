2762881904ff45057a2263638171a581979e59a7
SEKI-YUTA <zard.since1991@gmail.com>
2022-06-02 12:51:12 +0900

    add ref link and some code

--- test.py
+++ test.py
@@ -64,18 +64,27 @@ import numpy as np
 # 画像を完全一致か判定するプログラムここまで
 
 # 2枚の画像の差分画像を書き出すプログラム
-img1 = cv.imread("./assets/assetImg1.png")
-img2 = cv.imread("./assets/assetImg2.png")
+# img1 = cv.imread("./assets/assetImg1.png")
+# img2 = cv.imread("./assets/assetImg2.png")
 
-if img1.shape == img2.shape:
-    img_diff = img1.astype(int) - img2.astype(int)
-    img_diff_abs = np.abs(img_diff)
-    print(type(img1))
-    print(type(img_diff_abs))
-    # img_diff_abs_grey = cv.cvtColor(img_diff.asType(np.float32), cv.COLOR_BGR2GRAY)
-    print("最大値" + str(img_diff_abs.max()))
-    print("最小値" + str(img_diff_abs.min()))
-    # print(type(img_diff_abs_grey))
-    cv.imwrite("diff_image.png", img_diff_abs)
+# if img1.shape == img2.shape:
+#     img_diff = img1.astype(int) - img2.astype(int)
+#     img_diff_abs = np.abs(img_diff)
+#     print(type(img1))
+#     print(type(img_diff_abs))
+#     # img_diff_abs_grey = cv.cvtColor(img_diff.asType(np.float32), cv.COLOR_BGR2GRAY)
+#     print("最大値" + str(img_diff_abs.max()))
+#     print("最小値" + str(img_diff_abs.min()))
+#     # print(type(img_diff_abs_grey))
+#     cv.imwrite("diff_image.png", img_diff_abs)
     # cv.imshow("diff_image", img_diff_abs)
 # 2枚の画像の差分画像を書き出すプログラムここまで
+
+
+# 画像の色を判定するプログラム
+img = cv.imread("assets/assetImg3.png")
+b = img.T[0].flatten().mean()
+print(b)
+# https://qiita.com/Zumwalt/items/4d9bc15608483fa77476
+# print(img)
+# 画像の色を判定するプログラムここまで
